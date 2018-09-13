2fbb3f64dd0a389532e917b1db7446932390c0a6
Tsuneo Muller <tmul918@UoA.auckland.ac.nz>
2018-09-13 14:47:47 +1200

    First Commit
    Added Unity Project to Github

---
+++ ModelExplorer/Assets/Scripts/Astronaut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astronaut : MonoBehaviour {
+
+    public string AstronautId { get; set; }
+    public string AstronautName { get; set; }
+    public string Mission { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
